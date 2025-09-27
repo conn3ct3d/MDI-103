2ec5b4d93e5f6683c03be752538e187b9344f0b5
Emiliano Magana <websmithmxl@gmail.com>
2025-09-27 12:15:38 -0700

    Initial Commit

---
+++ Project103/Project103App.swift
@@ -0,0 +1,17 @@
+//
+//  Project103App.swift
+//  Project103
+//
+//  Created by mbair on 9/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Project103App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Project103Tests/Project103Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Project103Tests.swift
+//  Project103Tests
+//
+//  Created by mbair on 9/27/25.
+//
+
+import Testing
+@testable import Project103
+
+struct Project103Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
