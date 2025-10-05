4b1aed022d0260e14f4060b70933657a5191ad99
Emiliano Magana <websmithmxl@gmail.com>
2025-10-04 20:47:44 -0700

    Added course average functionality

--- Project103/Assignments/Assignment3.playground/Contents.swift
+++ Project103/Assignments/Assignment3.playground/Contents.swift
@@ -7,26 +7,60 @@ import UIKit
 //  Created by Emiliano on 10/4/25.
 //
 
-class StudentClass{
-    var name:String
-    var age:Int
-    var grades:[Double]
+class Course {
+    var courseName: String
+    var students: [StudentClass]
     
-    init(_ name:String, _ age:Int, _ grades:[Double]){
+    init(_ courseName: String) {
+        self.courseName = courseName
+        self.students = []
+    }
+    
+    func enrollStudent(_ student: StudentClass) {
+        students.append(student)
+        print("\(student.name) has been enrolled in \(courseName).")
+    }
+    
+    func listStudents() {
+        print("Students in \(courseName):")
+        for student in students {
+            print("- \(student.name), Age: \(student.age)")
+        }
+    }
+    
+    func getCourseAverage() -> Double {
+        if students.isEmpty {
+            return 0.0
+        }
+        
+        
+        let total = students.reduce(0.0) { partial, student in
+            partial + (student.grades.reduce(0,+) / Double(student.grades.count))
+        }
+        
+        return total / Double(students.count)
+    }
+}
+
+class StudentClass {
+    var name: String
+    var age: Int
+    var grades: [Double]
+    
+    init(_ name: String, _ age: Int, _ grades: [Double]) {
         self.name = name
         self.age = age
         self.grades = grades
     }
     
     func getAverageGrade() -> String {
-        if grades.isEmpty{
+        if grades.isEmpty {
             return "0.0"
         }
         let average = grades.reduce(0,+)/Double(grades.count)
         if average > 60 {
             return "\(name) has a passing grade of \(average)"
-        }
-        else {
+        } else {
             return "\(name) has a failing grade of \(average)"
         }
     }
@@ -35,7 +69,9 @@ class StudentClass{
 let student = StudentClass("Maria", 27, [100.0, 100.0, 94.0])
 let student2 = StudentClass("Lucas", 26, [50.00, 75.00, 49.00])
 
-print()
-print(student.getAverageGrade())
-print()
-print(student2.getAverageGrade())
+let course1 = Course("MDI-103")
+course1.enrollStudent(student)
+course1.enrollStudent(student2)
+
+course1.listStudents()
+print("Course average: \(course1.getCourseAverage())")
